cca4b25792f517c47bf53d316294c50443a8f82e
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-02 13:56:17 +0300

    styled Greeting by flex-box

--- src/Greeting.js
+++ src/Greeting.js
@@ -4,6 +4,17 @@ import styles from  './Greeting.module.css';
 function Greeting() {
   return (
     <div className={styles.greeting}>
+      <div className={styles.container}>
+        <div className={styles.greetingText}>
+          <span>Hello</span>
+          <span>I am Anna</span>
+          <span>Front-end developer </span>
+        </div>
+        <div className={styles.profilePhoto}>
+          <img src="" alt=""/>
+        </div>
+
+      </div>
 
     </div>
   );
