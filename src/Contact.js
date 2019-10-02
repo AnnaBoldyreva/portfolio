b0bcb7b941fbc571a31d63dacdc633ed4e9f73f2
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-02 17:23:02 +0300

    add flexbox styles for Contact component

--- src/Contact.js
+++ src/Contact.js
@@ -4,6 +4,19 @@ import styles from  './Contact.module.css';
 function Contact() {
   return (
     <div className={styles.contact}>
+      <div className={styles.container}>
+        <div className={styles.contactInfo}>
+          <span>Contact</span>
+          <input  type="text" name="name" placeholder="Name"/>
+          <input  type="text" name="email" placeholder="Email"/>
+          <textarea  placeholder='Your message'>
+          </textarea>
+          <button type='submit'>Send</button>
+
+
+        </div>
+
+      </div>
 
     </div>
   );
