06551f243c2d1eaf322892c72041174171277982
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-02 16:37:42 +0300

    add flexbox styles to component Project

--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,22 @@ import styles from  './Projects.module.css';
 function Projects() {
   return (
     <div className={styles.projects}>
+      <div className={styles.container}>
+        <div className={styles.firstProject}>
+          <img className={styles.imgProject} src="https://miro.medium.com/max/3118/1*iwPLQjyFYRTVeQ2cb4S9rA.png" alt="coding"/>
+          <span>Name of Project</span>
+          <span>Description</span>
+          <button className={styles.viewButton}>View</button>
+
+        </div>
+        <div className={styles.secondProject}>
+          <img className={styles.imgProject} src="https://marketingweek.imgix.net/content/uploads/2017/05/12103909/Coding-body-image-.jpg?auto=compress,format&q=60&w=750&h=460" alt="coding"/>
+          <span>Name of Project</span>
+          <span>Description</span>
+          <button className={styles.viewButton}>View</button>
+        </div>
+
+      </div>
 
     </div>
   );
