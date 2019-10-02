e0af3ff3f2d63ea65e10701f38c0c4fcca650739
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-02 16:06:39 +0300

    create Skills.module.css by flexbox

--- src/Skills.js
+++ src/Skills.js
@@ -4,6 +4,29 @@ import styles from  './Skills.module.css';
 function Skills() {
   return (
     <div className={styles.skills}>
+      <div className={styles.container}>
+        <div className={styles.firstSkill}>
+          <img className={styles.imgLogo} src="https://www.advicesexpress.ru/img/products/11092-vnesu-popravki-na-vashem-sajte-html-css.jpg" alt="htmlcss"/>
+          <span>HTML& CSS</span>
+          <span>Detailed description of skill</span>
+
+        </div>
+        <div className={styles.secondSkill}>
+            <img className={styles.imgLogo} src="https://itproger.com/img/courses/1476977754.jpg" alt="jslogo"/>
+            <span>JS</span>
+            <span>Detailed description of skill</span>
+
+
+        </div>
+        <div className={styles.thirdSkill}>
+
+            <img  className={styles.imgLogo} src="https://ionicframework.com/blog/wp-content/uploads/2019/02/react-beta.png" alt="jslogo"/>
+            <span>React</span>
+            <span>Detailed description of skill</span>
+
+        </div>
+
+      </div>
 
     </div>
   );
