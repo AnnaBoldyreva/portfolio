68754ce5f1999aee7eaa6a533fe126cd11e71664
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-10-13 18:37:26 +0300

    Updates

---
+++ precache-manifest.e88e93ec80a5e5e2c42c3d718449147a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d4974f17f9da26750b511951f6a646",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "27cd5c8f94cb5656b14a",
+    "url": "/portfolio/static/css/main.5ab20bfa.chunk.css"
+  },
+  {
+    "revision": "3e101cb4b377c4b5e606",
+    "url": "/portfolio/static/js/2.2d12f4bf.chunk.js"
+  },
+  {
+    "revision": "27cd5c8f94cb5656b14a",
+    "url": "/portfolio/static/js/main.ebed419d.chunk.js"
+  },
+  {
+    "revision": "a46b3fd8f34f8691f45e",
+    "url": "/portfolio/static/js/runtime-main.6c1b9ebb.js"
+  }
+]);
