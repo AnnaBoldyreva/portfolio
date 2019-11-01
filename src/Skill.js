aacbf812e7ef3392b654f89c8b6511d07b2cb99e
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-01 17:32:41 +0300

    refactoring

--- src/Skill.js
+++ src/Skill.js
@@ -4,9 +4,11 @@ import styles from './Skill.module.css'
 function Skill(props) {
   return (
    <div className={styles.firstSkill}>
+       <div className={styles.skill}>
        <img src={props.logo} alt={'logo'}/>
           <span>{props.name}</span>
           <span>{props.description}</span>
+       </div>
    </div>
 
 
