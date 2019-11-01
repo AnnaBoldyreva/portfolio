c7f761c4f4e67f70ee7a363b1fb75649c4252806
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-01 17:13:30 +0300

    media for projects

--- src/Contact.js
+++ src/Contact.js
@@ -5,8 +5,9 @@ function Contact() {
   return (
     <div className={styles.contact}>
       <div className={styles.container}>
-        <span className={styles.blockTitle}>Contact</span>
+
         <form className={styles.formWrapper}>
+          <span className={styles.blockTitle}>Contact</span>
 
           <input className={styles.formArea}  type="text" name="name" placeholder="Name"/>
           <input className={styles.formArea} type="text" name="email" placeholder="Email"/>
