10fc23b787eb41d2a0c1b0a716ac42725509f744
AnnaBoldyreva <annboldyrevaa@gmail.com>
2019-11-03 19:22:34 +0300

    styling greeting photo

--- src/Greeting.js
+++ src/Greeting.js
@@ -7,11 +7,13 @@ function Greeting() {
       <div className={styles.container}>
         <div className={styles.greetingText}>
           <span>Hello,</span>
-          <span>I am <span>Anna Boldyreva</span></span>
+          <span>I am Anna <span>Boldyreva</span></span>
           <span><h1>Front-end developer </h1></span>
         </div>
         <div className={styles.profilePhoto}>
-          <img src="" alt=""/>
+          <div className={styles.image}>
+            <img src='https://sun9-54.userapi.com/c855620/v855620556/14d1e5/xBK4LzNybE4.jpg'   alt=""/>
+          </div>
         </div>
 
       </div>
